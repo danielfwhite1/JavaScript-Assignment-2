e3a147badcf0e7a5c2d2a10ecb4dec12a2f57813
Daniel White <danielfwhite1@gmail.com>
2020-06-08 23:28:26 -0700

    second commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let isHotOutside = true;
+let isWeekday = true;
+let hasMoneyInPocket = true;
+
+// when to use const instead of let
+let costOfMilk = 3;
+let moneyInWallet = 10;
+let thirstLevel = 7;
+
+// why does assignment say boolean isSchoolDay = isWeekday && !isSummer;
+// and not var or let instead of boolean? confusing and code would not run in javascript.
+let shouldBuyIceCream = isHotOutside && hasMoneyInPocket;
+
+let willGoSwimming = isHotOutside && !isWeekday;
+
+let isAGoodDay = isHotOutside && hasMoneyInPocket && !isWeekday;
+
+let willBuyMilk = isHotOutside && thirstLevel>=3 && moneyInWallet>=2*costOfMilk;
+
+console.log(shouldBuyIceCream, willGoSwimming, isAGoodDay, willBuyMilk);
