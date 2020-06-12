d846dc63be1f95b8a9dba6b8f223fd1b14557ea1
Daniel White <danielfwhite1@gmail.com>
2020-06-12 16:01:50 -0700

    removed unnecessary comments

--- index.js
+++ index.js
@@ -2,13 +2,10 @@ let isHotOutside = true;
 let isWeekday = true;
 let hasMoneyInPocket = true;
 
-// const instead of let, all these change right?
 let costOfMilk = 3;
 let moneyInWallet = 10;
 let thirstLevel = 7;
 
-// why does assignment say boolean isSchoolDay = isWeekday && !isSummer;
-// and not var or let instead of boolean? confusing and code would not run in javascript.
 let shouldBuyIceCream = isHotOutside && hasMoneyInPocket;
 
 let willGoSwimming = isHotOutside && !isWeekday;
